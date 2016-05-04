ebd9b6abf55cfff4e4c35f1a8d86acae59441656
Wilfrido Vidana <woqer88@gmail.com>
2016-05-03 22:17:35 -0500

    nice usage message

--- easyfind/easyfind.sh
+++ easyfind/easyfind.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for var in $@
-do
-  find . -iname "*$var*" | grep --color=always $var
-done
+message(){
+  echo " - Find any file easily on your terminal"
+  echo " - Usage:"
+  echo "     easyfind [match_string1, match_string2 ...]"
+}
+
+if [ $# -eq 0 ]
+then
+  message
+else
+  for var in $@
+  do
+    find . -iname "*$var*" | grep --color=always $var
+  done
+fi
