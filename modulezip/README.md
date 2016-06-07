0b258b4a678c38220bf6816be6276fb0a8d584b3
Wilfrido Vidana <woqer88@gmail.com>
2016-06-07 16:14:47 -0500

    Now modulezip accepts libraries

--- modulezip/README.md
+++ modulezip/README.md
@@ -1,8 +1,8 @@
 ### Module zip
-Zip all python files and names the zip file after the module
+Zip a python project to work on Amazon Lambda
 
 ### Requirements
 Bash tool `zip` should be installed on the system
 
 ### Use
-`modulezip [module_name.py]`
+`modulezip <module_name.py> [library_folder_1, library_folder_2 ...]`
--- modulezip/modulezip.sh
+++ modulezip/modulezip.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 message(){
-  echo " - Easily zip a python module"
+  echo " - Zip a python project to work on Amazon Lambda"
   echo " - Usage:"
-  echo "     modulezip [module_name.py]"
+  echo "     $0 <module_name.py> [library_folder_1 library_folder_2 ...]"
 }
 
 if [ $# -eq 0 ]
 then
-  message
+  message $@
 else
   modulename=$(basename "$1")
   modulename="${modulename%.*}"
 
-  ls *.py | xargs zip "$modulename".zip
+  zip "$modulename".zip "$1"
+
+  shift
+
+  for lib in "$@"
+  do
+    zip -r "$modulename".zip "$lib"
+  done
 fi
